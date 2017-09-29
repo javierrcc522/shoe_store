f90efa081caa8e78582baeadcb3bab787f006fc5
Javier Jimenez <javierrcc522@gmail.com>
2017-09-29 13:47:18 -0700

    add 2 tables to add stores and brands and fix typo on the app

--- app.rb
+++ app.rb
@@ -4,19 +4,21 @@ Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
+# index
 get('/') do
   @brands = Brand.all()
   @stores = Store.all()
   erb(:index)
 end
 
+## brand table one
 get('/create_brand') do
   @brands = Brand.all()
   @stores = Store.all()
   erb(:index)
 end
 
-post('create_brand') do
+post('/create_brand') do
   brand = params['brand']
   price = params['price']
   brand = Brand.create({:brand => brand, :price => price})
@@ -24,17 +26,20 @@ post('create_brand') do
   @stores = Store.all()
   erb(:index)
 end
+### end of brand table
 
+## store table two
 get('/create_store') do
   @brands = Brand.all()
   @stores = Store.all()
   erb(:index)
 end
 
-post('create_store') do
+post('/create_store') do
   name = params['name']
   store = Store.create({:name => name})
   @stores = Store.all().sort_by {|brand| store.name}
   @brands = Brand.all()
   erb(:index)
 end
+## end of table two
