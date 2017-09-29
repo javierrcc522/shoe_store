d9191d65ad7cab1aed4a6526a772f2aa2808fc7b
Javier Jimenez <javierrcc522@gmail.com>
2017-09-29 11:37:44 -0700

    added brand_info and store_info to the views file also set up my app to show brands and stores in the index

--- app.rb
+++ app.rb
@@ -3,3 +3,38 @@ require 'pry'
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @brands = Brand.all()
+  @stores = Store.all()
+  erb(:index)
+end
+
+get('/create_brand') do
+  @brands = Brand.all()
+  @stores = Store.all()
+  erb(:index)
+end
+
+post('create_brand') do
+  brand = params['brand']
+  price = params['price']
+  brand = Brand.create({:brand => brand, :price => price})
+  @brands = Brand.all().sort_by {|brand| brand.price}
+  @stores = Store.all()
+  erb(:index)
+end
+
+get('/create_store') do
+  @brands = Brand.all()
+  @stores = Store.all()
+  erb(:index)
+end
+
+post('create_store') do
+  name = params['name']
+  store = Store.create({:name => name})
+  @stores = Store.all().sort_by {|brand| store.name}
+  @brands = Brand.all()
+  erb(:index)
+end
